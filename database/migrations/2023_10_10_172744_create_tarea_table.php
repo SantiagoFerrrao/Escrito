ccf3b4d0dae3a8d05a96a71caf7d6ba6e1561e22
Alejo <alejofocco09@gmail.com>
2023-10-10 14:30:45 -0300

    Tabla tarea

---
+++ database/migrations/2023_10_10_172744_create_tarea_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTareaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarea', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->timestamp('deleted_at');
+            $table->string('titulo');
+            $table->string('contenido');
+            $table->string('estado');
+            $table->string('autor');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarea');
+    }
+}
