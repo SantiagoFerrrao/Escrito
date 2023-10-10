f04ec8b583a39f7cb603c11b31e08d165099885c
Alejo <alejofocco09@gmail.com>
2023-10-10 14:44:15 -0300

    Rutas para funciones crud

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TareaController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/tareas', [TareaController::class, 'mostrarTareas']);
+Route::post('/tareas', [TareaController::class, 'crearTarea']);
+Route::patch('/tareas/{id}', [TareaController::class, 'modificarTarea']);
+Route::delete('/tareas/{id}', [TareaController::class, 'eliminarTarea']);
+Route::get('/tareas/buscar', [TareaController::class, 'buscarTarea']);
